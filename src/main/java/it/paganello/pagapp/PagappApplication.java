bbb77989148807fc37e76eef70dab7bfa98ada02
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-11-20 22:45:14 +0100

    first commit

---
+++ src/main/java/it/paganello/pagapp/PagappApplication.java
@@ -0,0 +1,13 @@
+package it.paganello.pagapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/paganello/pagapp/PagappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.paganello.pagapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
