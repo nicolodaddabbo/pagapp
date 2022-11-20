d726a261e550687f9b0f3c8ee47906552fec3404
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-11-20 23:07:39 +0100

    feat: created MatchingAlgorithm interface

---
+++ src/main/java/it/paganello/pagapp/matchingAlgorithm/MatchingAlgorithm.java
@@ -0,0 +1,10 @@
+package it.paganello.pagapp.matchingAlgorithm;
+
+import java.util.List;
+
+import it.paganello.pagapp.match.Match;
+import it.paganello.pagapp.round.Round;
+
+public interface MatchingAlgorithm {
+    Round computeNextRound(List<Match> playedMatches);
+}
