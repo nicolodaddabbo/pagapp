57580a3ad58196f1919b187a945d9ec2e43781a5
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-12-06 12:49:22 +0100

    refactor: match

--- src/main/java/it/paganello/pagapp/match/MatchController.java
+++ src/main/java/it/paganello/pagapp/match/MatchController.java
@@ -3,10 +3,12 @@ package it.paganello.pagapp.match;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,12 +20,23 @@ public class MatchController {
     MatchService service;
 
     @PutMapping(value = "/{id}")
-    public Optional<Match> modifyMatch(@PathVariable Long id, @RequestBody Match entity) {
-        return service.modifyMatch(id, entity);
+    public ResponseEntity<String> updateMatchResult(
+            @PathVariable final Long id, 
+            @RequestParam final int homeTeamPoints,
+            @RequestParam final int awayTeamPoints) {
+        Optional<String> error = service.updateMatchResult(id, homeTeamPoints, awayTeamPoints);
+        if (error.isPresent()) {
+            return new ResponseEntity<>(error.get(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Match results updated", HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public Optional<Match> getMatch(@PathVariable Long id) {
-        return service.getMatchById(id);
+    public ResponseEntity<Optional<Match>> getMatch(@PathVariable Long id) {
+        Optional<Match> match = service.getMatchById(id);
+        if (match.isEmpty()) {
+            return new ResponseEntity<>(Optional.empty(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(match, HttpStatus.OK);
     }
 }
--- src/main/java/it/paganello/pagapp/match/MatchService.java
+++ src/main/java/it/paganello/pagapp/match/MatchService.java
@@ -5,28 +5,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// import it.paganello.pagapp.round.RoundService;
-
 @Service
 public class MatchService {
     @Autowired
     MatchRepository repository;
-    // @Autowired
-    // RoundService roundService;
 
-    public Optional<Match> modifyMatch(final Long id, final Match newMatch) {
-        return repository.findById(id)
-                .map(match -> {
-                    match.setHomeTeam(newMatch.getHomeTeam());
-                    match.setHomeTeamPoints(newMatch.getHomeTeamPoints());
-                    match.setAwayTeam(newMatch.getAwayTeam());
-                    match.setField(newMatch.getField());
-                    match.setAwayTeamPoints(newMatch.getAwayTeamPoints());
-                    match.setFinished(newMatch.isFinished());
-                    Match result = repository.save(match);
-                    // roundService.checkRoundFinish(match.getRound());
-                    return result;
-                });
+    public Optional<String> updateMatchResult(final Long id, final int homeTeamPoints, final int awayTeamPoints) {
+        Optional<Match> match = repository.findById(id);
+        if (match.isEmpty()) {
+            return Optional.of("Match not found");
+        }
+        match.get().setHomeTeamPoints(homeTeamPoints);
+        match.get().setAwayTeamPoints(awayTeamPoints);
+        match.get().setFinished(true);
+        repository.save(match.get());
+        return Optional.empty();
     }
 
     public Match createMatch(final Match match) {
