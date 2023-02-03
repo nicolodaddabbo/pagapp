1ba7e677e17f8ffd2770a573dd2c3a1d23b0901a
ivan.notarstefano <R1sb33Fr1sb33M!>
2023-02-03 01:07:33 +0100

    Create new tournament names

--- src/main/java/it/paganello/pagapp/matchingAlgorithm/MatchingAlgorithmName.java
+++ src/main/java/it/paganello/pagapp/matchingAlgorithm/MatchingAlgorithmName.java
@@ -3,16 +3,18 @@ package it.paganello.pagapp.matchingAlgorithm;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum MatchingAlgorithmName {
-    KNOCKOUT("Knockout");
-
-    private final String name;
-
-    private MatchingAlgorithmName(final String name) {
-        this.name = name;
-    }
-
-    @JsonValue
-    public String getName() {
-        return this.name;
-    }
+	KNOCKOUT("Knockout"),
+	ROUNDROBIN("Round-Robin"),
+	SWISSDRAW("Swissdraw");
+	
+	private final String name;
+	
+	private MatchingAlgorithmName(final String name) {
+		this.name = name;
+	}
+	
+	@JsonValue
+	public String getName() {
+		return this.name;
+	}
 }
